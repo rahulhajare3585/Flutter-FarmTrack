2ed463fbb9c8791c15cf04b21c1909700861dc88
RahulHajare-Wai <rahul.hajare@waiin.com>
2024-10-09 12:22:39 +0530

    added forebase keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:farm_track/screens/home_screen.dart';
 import 'package:farm_track/screens/subscreens/expense_overview_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart'; // Add this import
 import 'package:farm_track/screens/Authentications/login_screen.dart';
 
 void main() {
+  if (kIsWeb) {
+    Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyD98wxOX-D2WzZF7GMur_nbXAvx5LKkgRE",
+            authDomain: "farmtrack-f0bd0.firebaseapp.com",
+            projectId: "farmtrack-f0bd0",
+            storageBucket: "farmtrack-f0bd0.appspot.com",
+            messagingSenderId: "320354338303",
+            appId: "1:320354338303:web:ef36fa6344f51effd113ca",
+            measurementId: "G-ZWNHBTGX96"));
+  } else {
+    Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
